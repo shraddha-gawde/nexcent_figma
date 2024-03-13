01ceb2ae0617243d09bab71be7d40882fbd3753d
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-03-13 21:22:15 +0530

    tailwind css started

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'silver-color': '#f5f7fa',
+        'primary-color':'#4caf4f',
+        'secondary-color': '#263238',
+        'text-text-gray-900': '#18191f'
+      },
+    },
+  },
+  plugins: [],
+}
