94958ce6e99e69d7f7d081118fdb5da788e3900d
shraddha-gawde <shraddha.gawde1999@gmail.com>
2024-03-13 21:58:39 +0530

    hero container created

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
         'silver-color': '#f5f7fa',
         'primary-color':'#4caf4f',
         'secondary-color': '#263238',
-        'text-text-gray-900': '#18191f'
+        'text-gray-900': '#18191f',
+        'dark-gray-color': '#4d4d4d',
+        'brand-primary-color':'#4caf4f',
+      },
+      container: {
+        center: true,
       },
     },
   },
